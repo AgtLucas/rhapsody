b04236a3a576a44ad579654e454d4a3489f61c91
Lucas <lucas@agtlucas.com>
2015-02-09 21:56:51 -0200

    Update gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  gulp.src('assets/sass/main.scss')
+    .pipe(sass({
+      style: 'compressed'
+    }))
+    .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('assets/sass/**.*', ['default']);
+});
+
+gulp.task('default', ['sass']);
